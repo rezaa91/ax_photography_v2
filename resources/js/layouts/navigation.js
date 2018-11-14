24672da6369c61423874055f82bbb63558ac9330
Ali Issaee <rezaa91@hotmail.co.uk>
2018-11-14 22:56:56 +0000

    Check if user is in session through api call

--- resources/js/layouts/navigation.js
+++ resources/js/layouts/navigation.js
@@ -2,7 +2,62 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class Navigation extends Component {
+    constructor() {
+        super();
+
+        //check if user is in session
+        const user = this.getUser();
+
+        this.state = {
+            isLoggedIn: false,
+            user_id: null,
+            name: null,
+            email: null,
+            created_at: null
+        }
+        
+        //bind methods
+        this.getUser = this.getUser.bind(this);
+        this.displayLoginOrUser = this.displayLoginOrUser.bind(this);
+    }
+
+    async getUser() {
+        //find user in session through api
+        await fetch('/user')
+        .then(response => response.json())
+        .then(data => {
+            data = data.data;
+            
+            if (data.length === 0) {
+                return;
+            }
+
+            this.setState({
+                isLoggedIn: true,
+                user_id: data.id,
+                name: data.name,
+                email: data.email,
+                created_at: data.created_at
+            })            
+        })
+    }
+
+    displayLoginOrUser() {
+        const {isLoggedIn, name} = this.state;
+
+        if (!isLoggedIn) {
+            return(
+                <li><a href='/login'>Login</a></li>
+            );
+        } else {
+            return(
+                <li><a href='/dashboard'>{name}</a></li>
+            );
+        }
+    }
+
     render() {
+        const {user_id, name, email, created_at} = this.state;
         return(
             <div className="navigation-wrapper">
                 <ul className="navigation navigation-left">
@@ -13,7 +68,7 @@ class Navigation extends Component {
                 </ul>
 
                 <ul className="navigation navigation-right">
-                    <li><a href='/login'>Login</a></li>
+                    {this.displayLoginOrUser()}
                 </ul>
             </div>
         );
