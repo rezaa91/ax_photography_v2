711bdeb62c6439d4f4ceeba6cf24323b3e94d3b4
Ali Issaee <rezaa91@hotmail.co.uk>
2018-11-18 17:40:30 +0000

    added empty auth react files

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,15 @@ require('./bootstrap');
 /** Require layouts */
 require('./layouts/navigation');
 
+/** Require Auth pages */
+require('./pages/auth/login');
+require('./pages/auth/register');
+require('./pages/auth/forgot_password');
+
 /** Require pages */
 require('./pages/homepage/homepage');
 require('./pages/albums/albums');
 require('./pages/about/about');
 require('./pages/contact/contact');
 require('./pages/upload/upload');
+require('./pages/dashboard/dashboard');
