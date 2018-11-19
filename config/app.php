2fd054ab408b1552133444307190237eda2ac170
Ali Issaee <rezaa91@hotmail.co.uk>
2018-11-19 22:07:26 +0000

    laravel collective html added

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -221,6 +222,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class
 
     ],
 
