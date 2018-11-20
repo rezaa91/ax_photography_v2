7c672fa6d5dd86d28a5121e2786b39db5f609170
Ali Issaee <rezaa91@hotmail.co.uk>
2018-11-20 21:50:56 +0000

    created form for user to edit profile

---
+++ resources/views/pages/profile/edit.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+
+{!! Form::open(['action' => ['DashboardController@update', $user->id], 'method' => 'POST']) !!}
+
+    <div class="form-section">
+    {{ Form::label('username') }}
+    {{ Form::text('username', $user->username) }}
+    </div>
+
+    <div class="form-section">
+    {{ Form::label('email') }}
+    {{ Form::text('email', $user->email) }}
+    </div>
+
+    <div class="form-section">
+    {{ Form::label('Current Password') }}
+    {{ Form::password('current_password') }}
+    </div>
+
+    <div class="form-section">
+    {{ Form::label('New Password') }}
+    {{ Form::password('new_password') }}
+    </div>
+
+    <div class="form-section">
+    {{ Form::label('Confirm Password') }}
+    {{ Form::password('confirm_password') }}
+    </div>
+
+    <div class="form-section">
+    {{ Form::submit('Update') }}
+    </div>
+
+{{ Form::hidden('_method', 'PUT') }}
+{!! Form::close() !!}
+
+@endsection
