22097fc3852192c9af818af16fcdae6fc0040c6b
Ali Issaee <rezaa91@hotmail.co.uk>
2018-11-22 21:05:10 +0000

    created basic view for about and contact pages

--- resources/js/pages/about/about.js
+++ resources/js/pages/about/about.js
@@ -4,8 +4,50 @@ import ReactDOM from 'react-dom';
 class About extends Component {
     render() {
         return(
-            <div>
+            <div className='about-wrapper'>
+
+                <div className='about-section'>
+                    <div className='about-header'>
+                        <h1>ABOUT</h1>
+                        <h3>Landscape Photographer.</h3>
+                    </div> 
                 
+                    <div className='about-body'>
+                        <div className='body-1'>
+                             <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et pharetra sem, eget fringilla libero. 
+                                Maecenas lobortis dapibus pellentesque. In convallis, ipsum et accumsan lobortis, ipsum lorem gravida 
+                                dolor, eu commodo ipsum justo quis ante. Vestibulum luctus, lorem venenatis dignissim tristique, ante 
+                                ante tristique neque, non maximus risus eros ut massa. Nulla dictum arcu eget elit malesuada aliquam. 
+                            </p>
+                            <p>
+                                Mauris pellentesque porta tristique. Suspendisse bibendum, libero vitae scelerisque laoreet, eros mauris 
+                                molestie orci, vitae consectetur lorem magna eget nibh. Pellentesque bibendum nibh enim, sit amet
+                                lacinia odio placerat eu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere 
+                                cubilia Curae; Nullam mattis libero eu risus faucibus tempor. Nulla turpis enim, cursus in quam sed, 
+                                sodales gravida elit. Praesent vestibulum odio erat, vitae fermentum tortor semper nec.
+                            </p> 
+                        </div>
+
+                        <div className='body-2'>
+                            {/* Profile image */}
+                            <img src="" />
+                        </div>
+                       
+                    </div>
+
+                    <div className='about-footer'>
+                        - Hamid Issaee
+                    </div>
+
+                </div> 
+                
+                <div className='carousel-wrapper'>
+                    <div className='carousel'>
+                        {/* Display carousel images here */}
+                    </div>
+                </div>
+
             </div>
         )
     }
--- resources/js/pages/contact/contact.js
+++ resources/js/pages/contact/contact.js
@@ -4,8 +4,47 @@ import ReactDOM from 'react-dom';
 class Contact extends Component {
     render() {
         return(
-            <div>
-                
+            <div className='contact-wrapper'>
+                <div className='contact-section'>
+
+                    <div className='contact-header'>
+                        <h1>CONTACT.</h1>
+                        <h3>Get in touch for a quote.</h3>                    
+                    </div>
+
+                    <div className='contact-body'>
+                        <div className='body-1'>
+                            <form>
+                                <div className="form-section">
+                                    <input type="text" placeholder="Name..." />
+                                </div>
+                                <div className='form-section'>
+                                    <input type="text" placeholder="Email@example.co.uk" />
+                                </div>
+                                <div className="form-section">
+                                    <textarea placeholder="Enter a message..."></textarea>
+                                </div>
+                                <div className="form-section">
+                                    <input type="submit" value="SEND" /> 
+                                </div>
+                            </form>
+                        </div>
+
+                        <div className='body-2'>
+
+                        </div>
+                    </div>
+
+                    <div className='contact-footer'>
+                        <div className='social-media-wrapper'>
+                            <span classname='icon'><a href="#"><span className="fab fa-flickr"></span></a></span>
+                            <span classname='icon'><a href="#"><span className="fab fa-twitter-square"></span></a></span>
+                            <span classname='icon'><a href="#"><span className="fab fa-instagram"></span></a></span>
+                            <span classname='icon'><a href="#"><span className="fab fa-facebook"></span></a></span>
+                        </div>
+                    </div>
+                    
+                </div>                
             </div>
         )
     }
