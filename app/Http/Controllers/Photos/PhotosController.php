faf31f4b8b22026063e1dc31f0bf63b8e77e0849
Ali Issaee <rezaa91@hotmail.co.uk>
2018-11-26 20:28:08 +0000

    Code refactored for handling image/album upload/creation

---
+++ app/Http/Controllers/Photos/PhotosController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Photos;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Photos;
+
+class PhotosController extends FileController 
+{
+    /**
+     * Store image in database
+     *
+     * @return void
+     */
+    public function storeImageInDatabase(Request $request, $album)
+    {
+        $photo = new Photos();
+        $photo->title = $request->input('title');
+        $photo->description = $request->input('description') || '';
+        $photo->album_id = $album['id'];
+        $photo->filepath = $this->getFilenameToStore();
+        $photo->created_at = Now();
+        $photo->user_id = auth()->user()->id;
+        
+        // Upload file to storage once inserted in to database
+        if ($photo->save()) {
+            $this->uploadFile();
+        }
+    }
+}   
