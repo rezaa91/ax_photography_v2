f59c857e2c9162094531f8405c5471dff61c4cad
Ali Issaee <rezaa91@hotmail.co.uk>
2018-11-26 20:29:39 +0000

    added album creation in upload view

--- resources/views/pages/photos/upload.blade.php
+++ resources/views/pages/photos/upload.blade.php
@@ -8,16 +8,39 @@
         <h1>Upload Images</h1>
     </div>
 
-    {!! Form::open(['action' => 'PhotosController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+    {!! Form::open(['action' => 'Photos\AlbumsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
 
     <div class='form-section'>
-        {{ Form::label('title') }}
+        {{ Form::label('title *') }}
         {{ Form::text('title', '') }}
     </div>
 
     <div class='form-section'>
         {{ Form::label('description') }}
-        {{ Form::textarea('description', '') }}
+        {{ Form::textarea('description', '', ['placeholder' => 'optional...']) }}
+    </div>
+
+    <div class='form-section'>
+        {{ Form::label('album *') }}
+        <div class='selection'>
+            <div class='option'>Existing: {{ Form::radio('album_type', 'existing', true) }}</div>
+            <div class='option'>New: {{ Form::radio('album_type', 'new', false) }}</div>
+        </div>
+    </div>
+
+    <div class="form-section" id="select_album">
+        @if ($albums)
+            <select name='album'>
+                <option value="default">Choose an album below...</option>
+                @foreach($albums as $album)
+                    <option value="{{ $album->album_id }}">{{ $album->album_name }}</option>
+                @endforeach
+            </select>
+        @endif
+    </div>    
+    
+    <div class='form-section hide' id="create_album">
+        {{ Form::text('create_album', '', ['placeholder' => 'Album Title...']) }}
     </div>
 
     <div class='form-section'>
