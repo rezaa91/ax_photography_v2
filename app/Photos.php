fce0e1f2be713c84e56e331e952f3a14423bc528
Ali Issaee <rezaa91@hotmail.co.uk>
2018-11-26 20:27:14 +0000

    Create albums and photos model with relationships

--- app/Photos.php
+++ app/Photos.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Albums;
 
 class Photos extends Model
 {
-    //
+    public function album()
+    {
+        return $this->hasOne(Albums::class);
+    }
 }
