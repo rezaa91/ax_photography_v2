b8123c512047bad6668b65bef3de7b12242d1ffc
Ali Issaee <rezaa91@hotmail.co.uk>
2018-12-02 20:01:13 +0000

    Display the individual album data

--- app/Http/Controllers/Photos/AlbumsController.php
+++ app/Http/Controllers/Photos/AlbumsController.php
@@ -113,12 +113,11 @@ class AlbumsController extends PhotosController
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show()
     {
-        
+        return view('pages.photos.singleAlbum');
     }
     
     /**
