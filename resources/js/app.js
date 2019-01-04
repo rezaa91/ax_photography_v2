562d9ad09ac65ff068b9dd94225ae8833baea9cd
Ali Issaee <rezaa91@hotmail.co.uk>
2019-01-04 22:05:08 +0000

    11 - create email functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,6 @@ require('./bootstrap');
 /** Require layouts */
 require('./layouts/navigation');
 
-/** Require Auth pages */
-require('./pages/auth/login');
-require('./pages/auth/register');
-require('./pages/auth/forgot_password');
-
 /** Require pages */
 require('./pages/homepage/homepage');
 require('./pages/albums/albums');
