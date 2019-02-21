fcf345646fec4bb82a29defad30a8069802c4836
Ali Issaee <rezaa91@hotmail.co.uk>
2019-02-21 20:54:26 +0000

    144 - resolved

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,15 @@
 
+import displayBottomNavBarMobile from './utilities/displayBottmNavBarMobile';
+
 /** Require dependencies and helpers */
 require('./bootstrap');
 
 /** Require layouts */
 require('./layouts/navigation');
+
+window.addEventListener('load', function() {
+    displayBottomNavBarMobile();
+})
 
 /** Require pages */
 require('./pages/homepage/homepage');
